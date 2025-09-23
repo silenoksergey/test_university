c5ce9de4d21cb19db35aa0b9252c7df992723fa3
skill <silenoksergey34@gmail.com>
2025-09-24 00:42:33 +0500

    fix name fixture

--- tests/test_grades.py
+++ tests/test_grades.py
@@ -4,21 +4,21 @@ faker = Faker()
 
 
 class TestGrades:
-    def test_grades_stats_count_is_two(self, _prepare_two_grades):
+    def test_grades_stats_count_is_two(self, prepare_two_grades):
         g1, g2, stats = _prepare_two_grades
         assert stats.count == 2, f"Wrong grades count. Expected '2', Actual: {stats.count}"
 
-    def test_grades_stats_min_correct(self, _prepare_two_grades):
-        g1, g2, stats = _prepare_two_grades
+    def test_grades_stats_min_correct(self, prepare_two_grades):
+        g1, g2, stats = prepare_two_grades
         expected = min(g1.grade, g2.grade)
         assert stats.min == expected, f"Wrong min grades. Expected: {expected}, Actual: {stats.min}"
 
-    def test_grades_stats_max_correct(self, _prepare_two_grades):
-        g1, g2, stats = _prepare_two_grades
+    def test_grades_stats_max_correct(self, prepare_two_grades):
+        g1, g2, stats = prepare_two_grades
         expected = max(g1.grade, g2.grade)
         assert stats.max == expected, f"Wrong max grades. Expected: {expected}, Actual: {stats.max}"
 
-    def test_grades_stats_avg_correct(self, _prepare_two_grades):
-        g1, g2, stats = _prepare_two_grades
+    def test_grades_stats_avg_correct(self, prepare_two_grades):
+        g1, g2, stats = prepare_two_grades
         expected = (g1.grade + g2.grade) / 2
         assert stats.avg == expected, f"Wrong avg grades. Expected: {expected}, Actual: {stats.avg}"
